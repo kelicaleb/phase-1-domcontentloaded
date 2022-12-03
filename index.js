0781ce13f63a73c4247a140c4f8f13399bd9d593
kelicaleb <kelicaleb7@gmail.com>
2022-12-03 18:02:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+// should change the text on the page when DOM content has loaded
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("text").textContent = "This is really cool!";
+});
